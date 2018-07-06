502740ed9843eb366c12f52d38fecbd7a6a9bd21
Chenming Jiang <chenmingjiang@Chenmings-MacBook-Pro.local>
2018-07-06 14:18:31 -0400

    Second Commit

--- src/main/java/com/gcit/springboot/DataController.java
+++ src/main/java/com/gcit/springboot/DataController.java
@@ -1,14 +1,24 @@
 package com.gcit.springboot;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @EnableWebSecurity
 @RestController
+@Repository("mysql")
 public class DataController {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+
     @RequestMapping("/")
     public String index() {
-        return "This is a spring boot application";
+        String sql = "SELECT name FROM Employee where id = ?";
+        String name = (String)jdbcTemplate.queryForObject(sql,new Object[] { 1 }, String.class);
+        return name;
     }
 }

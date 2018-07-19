266a6c9e5a1194a6ca64f2c209dfeff4cd07021b
Chenming Jiang <chenmingjiang@Chenmings-MacBook-Pro.local>
2018-07-19 14:00:59 -0400

    add target

--- src/main/java/com/gcit/springboot/SpringbootApplication.java
+++ src/main/java/com/gcit/springboot/SpringbootApplication.java
@@ -21,7 +21,7 @@ public class SpringbootApplication {
 //        return args -> {
 //
 //            System.out.println("Let's inspect the beans provided by Spring Boot:");
-
+//
 //            String[] beanNames = ctx.getBeanDefinitionNames();
 //            Arrays.sort(beanNames);
 //            for (String beanName : beanNames) {
--- src/test/java/com/gcit/springboot/SpringbootApplicationTests.java
+++ src/test/java/com/gcit/springboot/SpringbootApplicationTests.java
@@ -2,10 +2,13 @@ package com.gcit.springboot;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringRunner.class)
+
+@RunWith(SpringJUnit4ClassRunner.class)
+//@RunWith(MockitoJUnitRunner.class)
 @SpringBootTest
 public class SpringbootApplicationTests {
 
